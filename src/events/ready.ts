7eef8201e2fc3d5f474de14156458b10279b4c1a
Carter Nabors <carternabors12@gmail.com>
2020-05-19 02:41:50 -0400

    fixes the status changer

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -16,19 +16,16 @@ module.exports = class {
             `${client.user.tag}, ready to serve ${client.users.cache.size} users in ${client.guilds.cache.size} servers.`,
             "ready"
         );
-        const status = client.status,
+        const status = client.config.status,
             version = require("../../package.json").version;
         let i = 0;
         setInterval(function () {
             const toDisplay =
-                //@ts-ignore
-                status[parseInt(i, 10)].name.replace("{serversCount}", client.guilds.cache.size) +
+                status[i].name.replace("{serversCount}", client.guilds.cache.size) +
                 " | v" +
                 version;
-            //@ts-ignore
-            client.user.setActivity(toDisplay, { type: status[parseInt(i, 10)].type });
-            //@ts-ignore
-            if (status[parseInt(i + 1, 10)]) i++;
+            client.user.setActivity(toDisplay, { type: status[i].type });
+            if (status[i + 1]) i++;
             else i = 0;
         }, 20000); // Every 20 seconds
     }
